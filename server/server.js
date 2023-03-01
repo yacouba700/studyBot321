421580f042173bde6e1972a21947bbe2694cb93b
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-03-01 19:19:43 +0000

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from "express";
-
+import * as dotenv from "dotenv";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
-
+dotenv.config();
 
 const configuration = new Configuration({
-  apiKey:"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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
