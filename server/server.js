d499ae9300a1e505037ef97ca155b93491699249
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-03-01 14:42:58 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "",
 });
 
 const openai = new OpenAIApi(configuration);
